f820bf24e31bfd577a4dcf9b930da8db120876ed
nnaik39 <nanni.naik@gmail.com>
2023-11-29 11:01:59 -0800

    updated study coverage to only include questions with 2 hits already

--- trial_info/test_coverage.py
+++ trial_info/test_coverage.py
@@ -16,9 +16,11 @@ questions_per_image_context_pair = {}
 
 for participant in study_info:
     for trial in study_info[participant]:
-        if ((trial['picture'], trial['category']) not in questions_per_image_context_pair):
-            questions_per_image_context_pair[(trial['picture'], trial['category'])] = 0
-        questions_per_image_context_pair[(trial['picture'], trial['category'])] += 2
+        if ((trial['picture'], trial['category'], trial['question']) not in questions_per_image_context_pair):
+            questions_per_image_context_pair[(trial['picture'], trial['category'], trial['question'])] = 0
+        questions_per_image_context_pair[(trial['picture'], trial['category'], trial['question'])] += 1
+
+        print("Number of questions ", questions_per_image_context_pair[(trial['picture'], trial['category'], trial['question'])])
 
         if (trial['comments'] != ''):
             print(trial['comments'])
@@ -28,11 +30,15 @@ for participant in study_info:
 images_left = []
 
 for i in pilot_exp['images']:
-    if ((i['filename'], i['category']) in questions_per_image_context_pair and questions_per_image_context_pair[(i['filename'], i['category'])] >= 3):
-        print("Image ", i['filename'])
+    if ((i['filename'], i['category'], i['question']) in questions_per_image_context_pair and questions_per_image_context_pair[(i['filename'], i['category'], i['question'])] >= 3):
+        print("Excluding image ", i['filename'])
     else:
-        images_left.append((i['filename'], i['category']))
-        new_pilot_exp['images'].append(i)
+        if ((i['filename'], i['category'], i['question']) in questions_per_image_context_pair):
+            print("Number of hits left ", questions_per_image_context_pair[(i['filename'], i['category'], i['question'])])
+        images_left.append((i['filename'], i['category'], i['question']))
+
+        if ((i['filename'], i['category'], i['question']) in questions_per_image_context_pair and questions_per_image_context_pair[(i['filename'], i['category'], i['question'])] >= 2):
+            new_pilot_exp['images'].append(i)
 
 images_left = list(set(images_left))
 print("Number of images left ", len(images_left))
