398d6f569e4c790782bb9ca54d6e92dded576c20
nnaik39 <nanni.naik@gmail.com>
2023-11-29 12:22:59 -0800

    updated coverage info

--- trial_info/test_coverage.py
+++ trial_info/test_coverage.py
@@ -1,7 +1,7 @@
 import json 
 import random 
 
-f = open('pilot_exp.json')
+f = open('pilot_exp copy.json')
  
 # returns JSON object as
 # a dictionary
@@ -29,20 +29,33 @@ for participant in study_info:
 
 images_left = []
 
+covered_questions = 0
+
+# Add in ten questions with at least one hit here!
+
+num_one_hit_questions = 0
+
 for i in pilot_exp['images']:
     if ((i['filename'], i['category'], i['question']) in questions_per_image_context_pair and questions_per_image_context_pair[(i['filename'], i['category'], i['question'])] >= 3):
         print("Excluding image ", i['filename'])
+        covered_questions += 1
     else:
         if ((i['filename'], i['category'], i['question']) in questions_per_image_context_pair):
             print("Number of hits left ", questions_per_image_context_pair[(i['filename'], i['category'], i['question'])])
+
         images_left.append((i['filename'], i['category'], i['question']))
 
-        if ((i['filename'], i['category'], i['question']) in questions_per_image_context_pair and questions_per_image_context_pair[(i['filename'], i['category'], i['question'])] >= 2):
+        if ((i['filename'], i['category'], i['question']) in questions_per_image_context_pair and questions_per_image_context_pair[(i['filename'], i['category'], i['question'])] == 2):
             new_pilot_exp['images'].append(i)
+
+        if ((i['filename'], i['category'], i['question']) in questions_per_image_context_pair and questions_per_image_context_pair[(i['filename'], i['category'], i['question'])] == 1 and num_one_hit_questions < 10):
+            new_pilot_exp['images'].append(i)
+            num_one_hit_questions += 1
 
 images_left = list(set(images_left))
 print("Number of images left ", len(images_left))
 
+print("Number of covered questions ", covered_questions)
 json_object = json.dumps(new_pilot_exp, indent=4)
  
 # Writing to sample.json
