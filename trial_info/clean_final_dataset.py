9c3d18a0fe401824423c1857d24b48c3742f39df
nnaik39 <nanni.naik@gmail.com>
2024-01-22 21:08:25 -0800

    updated study coverage to new images

--- trial_info/clean_final_dataset.py
+++ trial_info/clean_final_dataset.py
@@ -1,12 +1,49 @@
 import json
 import random 
 
-f = open('dataset_annotations_so_far.json')
+#f = open('dataset_annotations_so_far.json')
+f = open('ig-vqa-default-rtdb-answer-elicitation-study-gpt4-descriptions-export.json')
+
 data = json.load(f)
 
 new_dataset_annotations = []
 
-for i in data:
+# First, just make a long list of image, context, description, question, and then answers
+#     {
+#        "image": "images/news/Showjumping_white_horse.jpeg",
+ #       "context": "social_media",
+ #       "description": "A horse and rider jumping over a white obstacle in an equestrian showjumping event, under a cloudy sky.",
+ #       "question": "what's the vibe, is it happy are they winning?",
+ #       "answers": [
+ #           "The vibe is more serious and determined ",
+ #           "The atmosphere is very passionate and active. However, it cannot be confirmed whether the match was won.",
+ #           "the person is happy, they are doing what they enjoy"
+ #       ]
+  #  },
+
+all_answers_per_question = {}
+
+for participant in data:
+    for trial in data[participant]:
+        image, context, description, question = trial['picture'], trial['category'], trial['description'], trial['question']
+        if ((image, context, description, question) not in all_answers_per_question):
+            all_answers_per_question[(image, context, description, question)] = []
+        all_answers_per_question[(image, context, description, question)].append(trial['answer'])
+
+full_answer_list = []
+
+# Create a full answer list from this!
+for (image, context, description, question) in all_answers_per_question:
+    if (len(all_answers_per_question) >= 5):
+        full_answer_list.append({
+            'image': image,
+            'context': context,
+            'description': description,
+            'question': question,
+            'answers': all_answers_per_question[(image, context, description, question)]
+        })
+
+for i in full_answer_list:
     num_unanswerable_votes = 0
 
     for answer in i['answers']:
