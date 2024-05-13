0358abbaba4315ba6dc38e7260d20863334680d5
nnaik39 <nanni.naik@gmail.com>
2024-05-13 15:09:08 -0700

    updated study coverage to 115 more images

---
+++ trial_info/check_progress.py
@@ -0,0 +1,5 @@
+# TODO: Output the number of unique image-context pairs
+# in the new_collected_dataset_so_far.json file!!
+
+# I need to reach 1,700 unique image-context pairs here.
+# And then I'm done!!
