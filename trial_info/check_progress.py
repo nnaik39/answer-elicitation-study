f7f35eae32e0db207cc327b2332e8ce08c16a76c
nnaik39 <nanni.naik@gmail.com>
2024-05-15 16:10:58 -0700

    updated study coverage (101 pairs)

--- trial_info/check_progress.py
+++ trial_info/check_progress.py
@@ -3,7 +3,7 @@ import random
 
 all_image_context_pairs = json.load(open('../../question-elicitation-study/trial_info/full_question_elicitation_study.json'))
 
-answer_study = json.load(open('/Users/nanditanaik/Downloads/ig-vqa-default-rtdb-answer-elicitation-study-dataset-expansion-export (60).json'))
+answer_study = json.load(open('/Users/nanditanaik/Downloads/ig-vqa-default-rtdb-answer-elicitation-study-dataset-expansion-export (61).json'))
 collected_dataset = json.load(open('new_collected_dataset_so_far.json'))
 
 image_context_pairs = []
